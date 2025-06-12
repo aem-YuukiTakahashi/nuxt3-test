c723ff021cb5094c8a3e1383ae1ad02cf95f58e2
aem-YuukiTakahashi <y.takahashi@axelentermedia.jp>
2025-06-12 13:10:14 +0900

    feat:nuxt3test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   app: {
     baseURL: '/nuxt3-test/' // 例: '/my-nuxt-app/'
   },
